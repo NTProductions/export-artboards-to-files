c3f5d7ac5a8f8139a9ba69739941f53b1ff9acbd
Nate Lovell <Natelovell99@gmail.com>
2021-01-07 11:53:39 -0700

    Create Export Artboards to Files.jsx

---
+++ Export Artboards to Files.jsx
@@ -0,0 +1,14 @@
+﻿// artboards to files
+
+var document = app.activeDocument;
+var boards = document.artboards;
+
+// loop through all artboards, make each one active and render before moving onto the next
+for(var i = 0; i < boards.length; i++) {
+    document.artboards.setActiveArtboardIndex(i);
+    var saveOptions = new IllustratorSaveOptions();
+    var aiDoc = new File("~/Documents/artboard_"+(i+1).toString());
+    saveOptions.saveMultipleArtboards = true;
+    saveOptions.artboardRange = (i+1).toString();
+    document.saveAs(aiDoc, saveOptions);
+    }
